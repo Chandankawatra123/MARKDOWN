16bdbf5e6020c746124ae67c37eab13eaf8309ad
Chandankawatra123 <100226305+Chandankawatra123@users.noreply.github.com>
2022-09-18 00:43:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const textEditor = document.querySelector(".text-editor");
+const preview = document.querySelector(".preview");
+function converter(){
+    let te = textEditor.value;
+    let prev = marked.parse(te);
+    preview.innerHTML=prev;
+}
+
+textEditor.addEventListener('input',converter);
